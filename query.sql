73c711e7e0cfc8e593a593341b74572a63aa2e4a
tomassgi <149562014+tomassgi@users.noreply.github.com>
2024-05-15 09:45:54 +0300

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,21 +1,112 @@
---
--- Docs
--- https://www.w3schools.com/sql/default.asp
---
--- Playground:
--- https://sqlfiddle.com/sqlite/online-compiler (MYSQL)
---
+CREATE TABLE cities (
+  title varchar(50),
+  year int,
+  population int,
+  region varchar(50)
+ );
+INSERT INTO cities (title, year, population, region) 
+VALUES ('Ādaži', 2022, 7482, 'Ādažu novads')
+,('Ainaži', 1926, 701, 'Limbažu novads')
+,('Aizkraukle', 1967, 7198, 'Aizkraukles novads')
+,('Aizpute', 1378, 4128, 'Dienvidkurzemes novads')
+,('Aknīste', 1991, 995, 'Jēkabpils novads')
+,('Aloja', 1992, 1073, 'Limbažu novads')
+,('Alūksne', 1920, 6945, 'Alūksnes novads')
+,('Ape', 1928, 832, 'Smiltenes novads')
+,('Auce', 1924, 2360, 'Dobeles novads')
+,('Baldone', 1991, 3789, 'Ķekavas novads')
+,('Baloži', 1991, 6992, 'Ķekavas novads')
+,('Balvi', 1928, 6103, 'Balvu novads')
+,('Bauska', 1609, 10202, 'Bauskas novads')
+,('Brocēni', 1992, 3009, 'Saldus novads')
+,('Cēsis', 1323, 16190, 'Cēsu novads')
+,('Cesvaine', 1991, 1280, 'Madonas novads')
+,('Dagda', 1992, 1887, 'Krāslavas novads')
+,('Daugavpils', 1582, 86872, 'Daugavpils')
+,('Dobele', 1917, 9031, 'Dobeles novads')
+,('Durbe', 1893, 520, 'Dienvidkurzemes novads')
+,('Grobiņa', 1695, 3778, 'Dienvidkurzemes novads')
+,('Gulbene', 1928, 7171, 'Gulbenes novads')
+,('Iecava', 2021, 5677, 'Bauskas novads')
+,('Ikšķile', 1992, 7698, 'Ogres novads')
+,('Ilūkste', 1917, 2312, 'Augšdaugavas novads')
+,('Jaunjelgava', 1647, 1825, 'Aizkraukles novads')
+,('Jēkabpils novads', 1670, 22134, 'Jēkabpils')
+,('Jelgava', 1573, 58154, 'Jelgavas valstspilsēta')
+,('Jūrmala', 1959, 56862, 'Jūrmalas valstspilsēta')
+,('Kandava', 1917, 3501, 'Tukuma novads')
+,('Kārsava', 1928, 1970, 'Ludzas novads')
+,('Koknese', 1277, 2562, 'Aizkraukles novads')
+,('Krāslava', 1923, 7771, 'Krāslavas novads')
+,('Kuldīga', 1378, 10395, 'Kuldīgas novads')
+,('Ķegums', 1993, 2147, 'Ogres novads')
+,('Ķekava', 2022, 5190, 'Ķekavas novads')
+,('Lielvārde', 1992, 6085, 'Ogres novads')
+,('Liepāja', 1625, 73333, 'Liepājas valstspilsēta')
+,('Līgatne', 1993, 1092, 'Cēsu novads')
+,('Limbaži', 1385, 7059, 'Limbažu novads')
+,('Līvāni', 1926, 7388, 'Līvānu novads')
+,('Lubāna', 1992, 1555, 'Madonas novads')
+,('Ludza', 1777, 7767, 'Ludzas novads')
+,('Madona', 1926, 7129, 'Madonas novads')
+,('Mazsalaca', 1928, 1143, 'Valmieras novads')
+,('Mārupe', 2022, 17796, 'Mārupes novads')
+,('Ogre', 1928, 24075, 'Ogres novads')
+,('Olaine', 1967, 10475, 'Olaines novads')
+,('Pāvilosta', 1991, 850, 'Dienvidkurzemes novads')
+,('Piltene', 1557, 877, 'Ventspils novads')
+,('Pļaviņas', 1927, 2950, 'Aizkraukles novads')
+,('Preiļi', 1928, 6326, 'Preiļu novads')
+,('Priekule', 1928, 1912, 'Dienvidkurzemes novads')
+,('Rēzekne', 1773, 28785, 'Rēzeknes valstspilsēta')
+,('Rīga', 1225, 655090, 'Rīgas valstspilsēta')
+,('Rūjiena', 1920, 2766, 'Valmieras novads')
+,('Sabile', 1917, 1430, 'Talsu novads')
+,('Salacgrīva', 1928, 2624, 'Limbažu novads')
+,('Salaspils', 1993, 18602, 'Salaspils novads')
+,('Saldus', 1917, 10168, 'Saldus novads')
+,('Saulkrasti', 1991, 3430, 'Saulkrastu novads')
+,('Seda', 1991, 1197, 'Valmieras novads')
+,('Sigulda', 1928, 14817, 'Siguldas novads')
+,('Skrunda', 1996, 1985, 'Kuldīgas novads')
+,('Smiltene', 1920, 5334, 'Smiltenes novads')
+,('Staicele', 1992, 861, 'Limbažu novads')
+,('Stende', 1991, 1605, 'Talsu novads')
+,('Strenči', 1928, 1042, 'Valmieras novads')
+,('Subate', 1917, 591, 'Augšdaugavas novads')
+,('Talsi', 1917, 9350, 'Talsu novads')
+,('Tukums', 1795, 17572, 'Tukuma novads')
+,('Valdemārpils', 1917, 1234, 'Talsu novads')
+,('Valka', 1584, 4935, 'Valkas novads')
+,('Valmieras novads', 1323, 24269, 'Valmieras novads')
+,('Vangaži', 1991, 3512, 'Ropažu novads')
+,('Varakļāni', 1928, 1755, 'Varakļānu novads')
+,('Ventspils', 1378, 35660, 'Ventspils valstspilsēta')
+,('Viesīte', 1928, 1530, 'Jēkabpils novads')
+,('Viļaka', 1945, 1245, 'Balvu novads')
+,('Viļāni', 1928, 2831, 'Rēzeknes novads')
+,('Zilupe', 1931, 1369, 'Ludzas novads');
 
--- Vārds Uzvārds - 
-
--- 1. Izveidot tabulu priekš faila cities.json datiem, izvēloties atbilstošus datu tipus (CREATE)
--- 2. Papildināt tabulu ar datiem no faila (INSERT) (drīkst izmantot konvertētāju https://www.convertsimple.com/convert-json-to-sql-insert-statement/)
--- 3. Atlasīt tikai nosaukumu un novadu kolonnas (SELECT)
--- 4. Atlasīt pilsētas kuras ir vecākas par 100 gadiem (WHERE)
--- 5. Atlasīt pilsētas ar mazāk vai vienādas ar 3501 iedzīvotāju skaitu (WHERE)
--- 6. Atlasīt top 10 lielākas pilsētas pēc iedzīvotāju skaita (LIMIT, ORDER BY)
--- 7. Saskaitīt kopējo latvijas pilsētu skaitu (COUNT)
--- 8. Atlasīt unikālus novada nosaukumus (DISTINCT)
--- 9. Dzēst ierakstu ar pilsētu “Durbe” (DELETE)
--- 10. Atlasīt kopēju iedzīvotāji skaitu pēc novadiem (GROUP BY, COUNT)
-
+-- 3.
+SELECT title, region FROM cities;
+-- 4.
+SELECT * FROM cities
+WHERE year<1924;
+-- 5.
+SELECT * FROM cities
+WHERE population<=3501;
+-- 6.
+SELECT * FROM cities
+ORDER BY population DESC
+LIMIT 10;
+-- 7.
+SELECT COUNT(title)
+FROM cities;
+-- 8.
+SELECT DISTINCT title FROM cities;
+-- 9.
+DELETE FROM cities WHERE title='Durbe';
+SELECT * FROM cities;
+-- 10.
+SELECT region, SUM(population) FROM cities
+GROUP BY region

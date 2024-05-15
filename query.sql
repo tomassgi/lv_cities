08fdfdf7b7b7dddcea0cb8ca0b5e0b0714da2aab
Ilya <karpov.ilja@gmail.com>
2024-05-15 08:37:22 +0300

    Task foundation

---
+++ query.sql
@@ -0,0 +1,21 @@
+--
+-- Docs
+-- https://www.w3schools.com/sql/default.asp
+--
+-- Playground:
+-- https://sqlfiddle.com/sqlite/online-compiler (MYSQL)
+--
+
+-- Vārds Uzvārds - 
+
+-- 1. Izveidot tabulu priekš faila cities.json datiem, izvēloties atbilstošus datu tipus (CREATE)
+-- 2. Papildināt tabulu ar datiem no faila (INSERT) (drīkst izmantot konvertētāju https://www.convertsimple.com/convert-json-to-sql-insert-statement/)
+-- 3. Atlasīt tikai nosaukumu un novadu kolonnas (SELECT)
+-- 4. Atlasīt pilsētas kuras ir vecākas par 100 gadiem (WHERE)
+-- 5. Atlasīt pilsētas ar mazāk vai vienādas ar 3501 iedzīvotāju skaitu (WHERE)
+-- 6. Atlasīt top 10 lielākas pilsētas pēc iedzīvotāju skaita (LIMIT, ORDER BY)
+-- 7. Saskaitīt kopējo latvijas pilsētu skaitu (COUNT)
+-- 8. Atlasīt unikālus novada nosaukumus (DISTINCT)
+-- 9. Dzēst ierakstu ar pilsētu “Durbe” (DELETE)
+-- 10. Atlasīt kopēju iedzīvotāji skaitu pēc novadiem (GROUP BY, COUNT)
+
